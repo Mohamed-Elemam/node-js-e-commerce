db3f67b536c46e96ac9fd04b211ffb06cc777c22
Mohamed-Elemam <mohamed.elemam.younis@gmail.com>
2023-08-16 23:50:42 +0300

    add

--- src/modules/brand/brand.controller.js
+++ src/modules/brand/brand.controller.js
@@ -1,25 +1,31 @@
 import slugify from "slugify";
 import { brandModel } from './../../../database/models/brand.model.js';
+import { categoriesModel } from "../../../database/models/categories.model.js";
+import { subCategoriesModel } from "../../../database/models/subCategories.model.js";
 
 //*------------
 //*1--add brand
 //*------------
 const addBrands = async (req, res, next) => {
   const { name } = req.body;
+  const { subCategoryId, categoryId } = req.query
+  const category = await categoriesModel.findById(categoryId);
+  if (!category) {
+    return res.status(404).json({ message: "category not found" })
+  }
 
-  const {  subCategoryId, categoryId   }=req.params
-
-  const category = await brandModel.findOne({ categoryId });
-  if(!category) return res.status(404).json({ message: "category not found" });
-  
-  const subCategory = await brandModel.findOne({ subCategoryId });
-  if(!subCategory) return res.status(404).json({ message: "subCategory not found" });
+  const subCategory = await subCategoriesModel.findById(subCategoryId);
+  if (!subCategory) {
+    return res.status(404).json({ message: "subCategory not found" })
+  }
 
   const isExist = await brandModel.findOne({ name });
 
-  if(isExist) return  res.status(400).json({ message: "brand already exist" });
+  if (isExist) {
+    return res.status(400).json({ message: "brand already exist" })
+  }
 
-  const newBrand = new brandModel({ name, slug: slugify(name),categoryId,subCategoryId });
+  const newBrand = new brandModel({ name, slug: slugify(name), categoryId, subCategoryId });
   await newBrand.save();
 
   res.status(201).json({ message: "brand add seccessfully", newBrand });
@@ -32,13 +38,13 @@ const updateBrands = async (req, res, next) => {
   const { _id } = req.params;
   const { name } = req.body;
 
-  const brand = await brandModel.findByIdAndUpdate(_id , { name , slug:slugify(name)}, {new:true});
-if(!brand){
-  return res.status(400).json({ message: "brand not found" });
-}
-if( brand){
-  return res.status(201).json({ message: "brand updated seccessfully" , brand});
-}
+  const brand = await brandModel.findByIdAndUpdate(_id, { name, slug: slugify(name) }, { new: true });
+  if (!brand) {
+    return res.status(400).json({ message: "brand not found" });
+  }
+  if (brand) {
+    return res.status(201).json({ message: "brand updated seccessfully", brand });
+  }
 };
 
 //*------------
@@ -47,13 +53,13 @@ if( brand){
 const deleteBrands = async (req, res, next) => {
   const { _id } = req.params;
 
-  const brand = await brandModel.findByIdAndDelete(_id );
-if(!brand){
-  return res.status(400).json({ message: "brand not found" });
-}
-if( brand){
-  return res.status(201).json({ message: "brand deleted seccessfully" });
-}
+  const brand = await brandModel.findByIdAndDelete(_id);
+  if (!brand) {
+    return res.status(400).json({ message: "brand not found" });
+  }
+  if (brand) {
+    return res.status(201).json({ message: "brand deleted seccessfully" });
+  }
 };
 
 //*------------
@@ -62,7 +68,7 @@ if( brand){
 const getAllBrands = async (req, res, next) => {
 
   const brands = await brandModel.find()
- res.status(201).json({ brands});
+  res.status(201).json({ brands });
 
 };
 
