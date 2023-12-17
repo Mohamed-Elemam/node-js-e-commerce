d2fbb61281e7ad146eee8dfd8c86aaa39e568315
Mohamed Elemam <mohamed.elemam.younis@gmail.com>
2023-12-17 19:57:00 +0200

    updated

--- src/modules/order/order.routes.js
+++ src/modules/order/order.routes.js
@@ -1,15 +1,12 @@
 import { Router } from "express";
 const router = Router();
-import * as ordertController from "./order.controller.js";
+import * as orderController from "./order.controller.js";
 import { errorHandling } from "../../../utils/errorHandling.js";
-import { handleAuth } from '../auth/auth.controller.js';
+import { handleAuth } from "../auth/auth.controller.js";
 
+router.post("/:id", handleAuth, errorHandling(orderController.createCashOrder));
 
-
-router.post("/:id",handleAuth,errorHandling(ordertController.createCashOrder))
-
-router.get("/",handleAuth,errorHandling(ordertController.getUserOrders))
-
+router.get("/", handleAuth, errorHandling(orderController.getUserOrders));
 
 // router.get("/",handleAuth,errorHandling(ordertController.getAllUserordert))
 export default router;
