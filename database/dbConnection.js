9c5c9d08efc05f602272fe954ccec5ed5cf1258b
Mohamed Elemam <mohamed.elemam.younis@gmail.com>
2023-12-17 19:57:14 +0200

    updated

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,14 +1,13 @@
+import mongoose from "mongoose";
 
-  import mongoose from "mongoose";
-
-
-
-
-  export const  dbConnection = async ()=>{
-  
+export const dbConnection = async () => {
   // await mongoose.connect("mongodb://127.0.0.1:27017/eCommerce")
-  await mongoose.connect(process.env.DB_CONNECTION_ONLINE)
-  .then(()=>{console.log('database connect successfully to ' )})
-  .catch((err)=>{console.log('database connection failed',err)})
-  }
-  
+  await mongoose
+    .connect(process.env.DB_CONNECTION_ONLINE)
+    .then(() => {
+      console.log("connected successfully to database");
+    })
+    .catch((err) => {
+      console.log("database connection failed", err);
+    });
+};
