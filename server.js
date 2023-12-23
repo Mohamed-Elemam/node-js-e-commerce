11107560770dd3bd82019eb2d61e3a6dfba99649
Mohamed Elemam <mohamed.elemam.younis@gmail.com>
2023-12-23 21:09:08 +0200

    add cors package to api

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-import express from 'express'
 import { config } from "dotenv";
 config();
-import { dbConnection } from './database/dbConnection.js'
-import { allRouters } from './src/modules/index.routers.js';
+import express from "express";
+import cors from "cors";
+import { dbConnection } from "./database/dbConnection.js";
+import { allRouters } from "./src/modules/index.routers.js";
+app.use(cors());
 
-const app = express()
-const port = process.env.PORT 
-app.use(express.json()) 
+const app = express();
+const port = process.env.PORT;
+app.use(express.json());
 
-dbConnection()
-allRouters(app)
+dbConnection();
+allRouters(app);
 
-
-
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
